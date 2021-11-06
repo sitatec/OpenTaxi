f6c16479c244a2e200df3b5adc8df0a88ec2c00a
Sita Bérété <sita.berete.3@gmail.com>
2021-11-06 12:00:06 +0000

    [dis] create Firebase Real-time Database's Rest API client

---
+++ source_code/backend/dispatcher/src/main/kotlin/com/hamba/dispatcher/services/api/FirebaseDatabaseClient.kt
@@ -0,0 +1,91 @@
+package com.hamba.dispatcher.services.api
+
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class FirebaseDatabaseClient(
+    private val httpClient: HttpClient,
+    private val baseUrl: String = "https://hamba-project-default-rtdb.firebaseio.com/"
+) {
+    suspend fun getData(queryBuilder: QueryBuilder): String {
+        val url = baseUrl + queryBuilder.build()
+        return withContext(Dispatchers.IO) {
+            httpClient.get(url)
+        }
+    }
+
+    suspend fun putData(queryBuilder: QueryBuilder, jsonData: String) {
+        val url = baseUrl + queryBuilder.build()
+        withContext(Dispatchers.IO) {
+            httpClient.put<HttpResponse>(url){
+                body = jsonData
+            }
+        }
+    }
+
+    suspend fun patchData(queryBuilder: QueryBuilder, jsonData: String) {
+        val url = baseUrl + queryBuilder.build()
+        withContext(Dispatchers.IO) {
+            httpClient.patch<HttpResponse>(url){
+                body = jsonData
+            }
+        }
+    }
+
+    suspend fun deleteData(queryBuilder: QueryBuilder) {
+        val url = baseUrl + queryBuilder.build()
+        withContext(Dispatchers.IO) {
+            httpClient.delete<HttpResponse>(url)
+        }
+    }
+
+    fun release() = httpClient.close()
+
+    class QueryBuilder(private var path: String) {
+        init {
+            path += ".json?"
+        }
+
+        fun orderBy(child: String) = apply {
+            path += "orderBy=$child&"
+        }
+
+        fun limitToFirst(n: Int) = apply {
+            path += "limitToFirst=$n&"
+        }
+
+        fun limitToLast(n: Int) = apply {
+            path += "limitToLast=$n&"
+        }
+
+        fun startAt(value: Number) = apply {
+            path += "startAt=$value&"
+        }
+
+        fun startAt(value: String) = apply {
+            path += "startAt=$value&"
+        }
+
+        fun endAt(value: Number) = apply {
+            path += "endAt=$value&"
+        }
+
+        fun endAt(value: String) = apply {
+            path += "endAt=$value&"
+        }
+
+        fun equalTo(value: Number) = apply {
+            path += "equalTo=$value&"
+        }
+
+        fun equalTo(value: String) = apply {
+            path += "equalTo=$value&"
+        }
+
+        fun build() = path.dropLast(1) // Remove the last & character.
+
+    }
+}
