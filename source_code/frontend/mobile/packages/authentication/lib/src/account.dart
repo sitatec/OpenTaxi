d7f8b2921123b52fd4c6bfc63440dc76361e521c
Sita Bérété <sita.berete.3@gmail.com>
2021-11-12 18:46:16 +0000

    [auth] create account, account role, and account status

---
+++ source_code/frontend/mobile/packages/authentication/lib/src/account.dart
@@ -0,0 +1,37 @@
+class Account {
+  final String id;
+  String firstName;
+  String lastName;
+  String nickname;
+  String email;
+  String phoneNumber;
+  String notificationToken;
+  final DateTime registeredAt;
+  final AccountRole role;
+  AccountStatus status;
+  double balance;
+
+  Account({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.phoneNumber,
+    required this.registeredAt,
+    required this.role,
+    required this.status,
+    required this.balance,
+    this.nickname = "",
+    this.notificationToken = "",
+  });
+}
+
+enum AccountRole { driver, rider, admin }
+
+enum AccountStatus {
+  live,
+  waitingForApproval,
+  unpaidSubscription,
+  temporarilySuspended,
+  definitivelyBanned
+}
