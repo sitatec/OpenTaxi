7b72986aadf7f2b207184bc61bea8dda27b163a1
Sita Bérété <sita.berete.3@gmail.com>
2021-12-04 00:05:26 +0000

    [sw] create Gender Widget and refactor buildTextField fn to OutLinedTextField class

--- source_code/frontend/mobile/packages/shared_widgets/lib/shared_widgets.dart
+++ source_code/frontend/mobile/packages/shared_widgets/lib/shared_widgets.dart
@@ -2,3 +2,4 @@ library shared_widgets;
 
 export 'src/colors.dart';
 export 'src/simple_widgets.dart';
+export 'package:flutter_svg/flutter_svg.dart';
--- source_code/frontend/mobile/packages/shared_widgets/lib/src/simple_widgets.dart
+++ source_code/frontend/mobile/packages/shared_widgets/lib/src/simple_widgets.dart
@@ -1,37 +1,147 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shared_widgets/shared_widgets.dart';
 
-TextField buildTextField({
-  required ValueChanged<String> onChanged,
-  Widget? prefixIcon,
-  Color? fillColor,
-  String? hintText,
-  double borderRadius = 10,
-  Color borderColor = gray,
-  EdgeInsets prefixPadding = const EdgeInsets.only(left: 15, right: 13),
-}) {
-  return TextField(
-    style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 17),
-    decoration: InputDecoration(
-      contentPadding: const EdgeInsets.symmetric(vertical: 16.5),
-      prefixIcon: Padding(
-        padding: prefixPadding,
-        child: prefixIcon,
-      ),
-      prefixIconConstraints: const BoxConstraints(
-        minHeight: 24,
-        minWidth: 24,
-      ),
-      fillColor: fillColor,
-      hintText: hintText,
-      filled: fillColor != null,
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(borderRadius),
-        borderSide: BorderSide(
-          color: borderColor,
-          width: 0.6,
+class OutLinedTextField extends StatelessWidget {
+  final ValueChanged<String> onChanged;
+  final Widget? prefixIcon;
+  final Color? fillColor;
+  final String? hintText;
+  final double borderRadius;
+  final Color borderColor;
+  final EdgeInsets prefixPadding;
+
+  const OutLinedTextField({
+    Key? key,
+    required this.onChanged,
+    this.prefixIcon,
+    this.fillColor,
+    this.hintText,
+    this.borderRadius = 10,
+    this.borderColor = gray,
+    this.prefixPadding = const EdgeInsets.only(left: 15, right: 13),
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 17),
+      decoration: InputDecoration(
+        contentPadding: const EdgeInsets.symmetric(vertical: 16.5),
+        prefixIcon: Padding(
+          padding: prefixPadding,
+          child: prefixIcon,
+        ),
+        prefixIconConstraints: const BoxConstraints(
+          minHeight: 24,
+          minWidth: 24,
+        ),
+        fillColor: fillColor,
+        hintText: hintText,
+        filled: fillColor != null,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(borderRadius),
+          borderSide: BorderSide(
+            color: borderColor,
+            width: 0.6,
+          ),
         ),
       ),
-    ),
-  );
+    );
+  }
+}
+
+class RoundedCornerButton extends StatelessWidget {
+  Color? disabledColor, enabledColor;
+  final VoidCallback? onPressed;
+  final Widget child;
+
+  RoundedCornerButton(
+      {Key? key,
+      this.disabledColor,
+      this.enabledColor,
+      this.onPressed,
+      this.child = const Text(
+        "Continue",
+        style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w700,
+          color: Colors.white,
+        ),
+        textAlign: TextAlign.center,
+      )})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    disabledColor ??= theme.disabledColor;
+    enabledColor ??= theme.primaryColor;
+    return SizedBox(
+      height: 54,
+      child: TextButton(
+        onPressed: onPressed,
+        child: SizedBox(
+          width: double.infinity,
+          child: child,
+        ),
+        style: ButtonStyle(
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          backgroundColor: MaterialStateProperty.resolveWith(
+            (states) => states.contains(MaterialState.disabled)
+                ? disabledColor
+                : enabledColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Gender extends StatelessWidget {
+  final String genderName;
+  final Color? backgroundColor;
+  final VoidCallback? onClicked;
+
+  const Gender(this.genderName,
+      {Key? key, this.backgroundColor, this.onClicked})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton.icon(
+      onPressed: onClicked,
+      icon: SvgPicture.asset(
+        "assets/images/${genderName.toLowerCase()}.svg",
+        package: "shared_widgets",
+      ),
+      label: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Text(
+          genderName[0].toUpperCase() + genderName.substring(1),
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(lightGray),
+        shape: MaterialStateProperty.all(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+            side: const BorderSide(
+              color: Color(0xFFACACAC),
+              width: 0.6,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
