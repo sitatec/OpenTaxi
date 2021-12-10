3789d95d9cb01c28d27fa6740b05baa2b77d8855
Sita Bérété <sita.berete.3@gmail.com>
2021-12-10 18:43:33 +0000

    [shared] create enum utils

--- source_code/frontend/mobile/packages/shared/lib/shared.dart
+++ source_code/frontend/mobile/packages/shared/lib/shared.dart
@@ -2,8 +2,9 @@ library shared;
 
 export 'src/constants/colors.dart';
 export 'src/constants/values.dart';
+export 'src/utils/enum_utils.dart';
 export 'src/simple_widgets.dart';
 // ----------------------- Exported Packages -------------------------- //
 export 'package:flutter_svg/flutter_svg.dart';
-export 'package:flutter_multi_formatter/flutter_multi_formatter.dart';
+export 'package:flutter_multi_formatter/flutter_multi_formatter.dart' hide enumFromString, enumToString;
 export 'package:data_access/data_access.dart';
