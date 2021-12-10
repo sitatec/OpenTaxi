acd3c5fccc63567797e473c67b0fe847884a2831
Sita Bérété <sita.berete.3@gmail.com>
2021-12-10 15:30:38 +0000

    [dri] add a generic loading widget to the RegistrationFormTemplate

--- source_code/frontend/mobile/apps/driver_app/lib/authentication/ui/registration_form_template.dart
+++ source_code/frontend/mobile/apps/driver_app/lib/authentication/ui/registration_form_template.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared/shared.dart';
 
-class RegistrationFormTemplate extends StatelessWidget {
+class RegistrationFormTemplate extends StatefulWidget {
   final Widget child;
   final VoidCallback? onContinue;
   final String title, subtitle;
@@ -15,35 +15,79 @@ class RegistrationFormTemplate extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<RegistrationFormTemplate> createState() =>
+      _RegistrationFormTemplateState();
+}
+
+class _RegistrationFormTemplateState extends State<RegistrationFormTemplate> {
+  String _loadingMessage = "";
+
+  void setLoadingMessage(String isSavingData) =>
+      setState(() => _loadingMessage = isSavingData);
+
+  @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
           elevation: 0, backgroundColor: Colors.transparent, toolbarHeight: 35),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child:
-                    SvgPicture.asset("assets/images/introduce_your_self.svg"),
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: SvgPicture.asset(
+                        "assets/images/introduce_your_self.svg"),
+                  ),
+                  const SizedBox(height: 50),
+                  Text(
+                    widget.title,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.w500, fontSize: 24),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(widget.subtitle, style: const TextStyle(fontSize: 16)),
+                  const SizedBox(height: 30),
+                  widget.child,
+                  RoundedCornerButton(onPressed: widget.onContinue),
+                  const SizedBox(height: 30),
+                ],
               ),
-              const SizedBox(height: 50),
-              Text(
-                title,
-                style:
-                    const TextStyle(fontWeight: FontWeight.w500, fontSize: 24),
-              ),
-              const SizedBox(height: 8),
-              Text(subtitle, style: const TextStyle(fontSize: 16)),
-              const SizedBox(height: 30),
-              child,
-              RoundedCornerButton(onPressed: onContinue),
-              const SizedBox(height: 30),
-            ],
+            ),
           ),
-        ),
+          if (_loadingMessage.isNotEmpty) ...[
+            Container(color: const Color(0x70000000)),
+            Align(
+              alignment: Alignment.center,
+              child: Wrap(
+                // width: min(screenWidth * 0.75, 350),
+                // height: 350,
+                children: [
+                  Card(
+                    elevation: 10,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 25),
+                      child: Column(
+                        children: [
+                          CircularProgressIndicator(color: theme.primaryColor),
+                          const SizedBox(height: 30),
+                          Text(_loadingMessage)
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ],
       ),
     );
   }
