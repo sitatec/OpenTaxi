7859d088f8b251a614638e677678f886e5c4c980
Sita Bérété <sita.berete.3@gmail.com>
2021-12-14 19:31:45 +0000

    [dri] Completes the dispatcher
      - Manage data flow: now the dispatcher can receive and process data,
        as well as sending data to the dispatcher server.

--- source_code/frontend/mobile/apps/driver_app/lib/entities/dispatcher.dart
+++ source_code/frontend/mobile/apps/driver_app/lib/entities/dispatcher.dart
@@ -1,29 +1,70 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:shared/shared.dart';
 import 'package:web_socket_channel/status.dart' as close_reason;
 
-const dispatcherServerUrl = "wss://dispatcher-dot-hamba-project.uc.r.appspot.com/driver";
+const dispatcherServerUrl =
+    "wss://dispatcher-dot-hamba-project.uc.r.appspot.com/driver";
 
 class Dispatcher {
   WebSocketChannel? _webSocketChannel;
   final _connectionStreamController = StreamController<bool>();
   bool _isConnected = false;
   Stream<bool> get isConnected => _connectionStreamController.stream;
+  Stream<MapEntry<FramType, dynamic>>? get dataStream =>
+      _webSocketChannel?.stream.map(_convertData);
 
   Dispatcher([this._webSocketChannel]);
 
+  MapEntry<FramType, dynamic> _convertData(data) {
+    data as String;
+    final separatorIndex = data.indexOf(":");
+    final code = int.parse(data.substring(0, separatorIndex));
+    final message = data.substring(separatorIndex + 1);
+    return MapEntry(FramType.values[code], message);
+  }
+
   Future<void> connect() async {
-    if(_isConnected) return;
-    _webSocketChannel = WebSocketChannel.connect(Uri.parse(dispatcherServerUrl));
+    if (_isConnected) return;
+    _webSocketChannel =
+        WebSocketChannel.connect(Uri.parse(dispatcherServerUrl));
     _connectionStreamController.add(true);
     _isConnected = true;
   }
 
   Future<void> disconnect() async {
-    if(!_isConnected) return;
+    if (!_isConnected) return;
     await _webSocketChannel?.sink.close(close_reason.goingAway);
     _connectionStreamController.add(false);
     _isConnected = false;
   }
+
+  void sendData(MapEntry<FramType, dynamic> data) {
+    _webSocketChannel?.sink.add(_dataToString(data));
+  }
+
+  String _dataToString(MapEntry<FramType, dynamic> data) {
+    String dataValue = data.value is Map ? jsonEncode(data.value) : data.value;
+    return "${data.key.index}:$dataValue";
+  }
+}
+
+enum FramType {
+// There are some values we don't use, but we keep them in order to preserve the
+//correct index for each value..
+  BOOKING_REQUEST,
+  ADD_DRIVER_DATA,
+  UPDATE_DRIVER_DATA,
+  DELETE_DRIVER_DATA,
+  ACCEPT_BOOKING,
+  REFUSE_BOOKING,
+  DISPATCH_REQUEST,
+  CANCEL_BOOKING,
+  INVALID_DISPATCH_ID,
+  NO_MORE_DRIVER_AVAILABLE,
+  PAIR_DISCONNECTED,
+  BOOKING_REQUEST_TIMEOUT,
+  BOOKING_SENT,
+  TRIP_ROOM,
 }
