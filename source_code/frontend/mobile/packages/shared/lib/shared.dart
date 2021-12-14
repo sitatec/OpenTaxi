871c84dc7be59bf3e460ba2eec2d5ecb5c1280ac
Sita Bérété <sita.berete.3@gmail.com>
2021-12-14 23:10:14 +0000

    [shared] export location_manager

--- source_code/frontend/mobile/packages/shared/lib/shared.dart
+++ source_code/frontend/mobile/packages/shared/lib/shared.dart
@@ -6,7 +6,9 @@ export 'src/utils/enum_utils.dart';
 export 'src/simple_widgets.dart';
 // ----------------------- Exported Packages -------------------------- //
 export 'package:flutter_svg/flutter_svg.dart';
-export 'package:flutter_multi_formatter/flutter_multi_formatter.dart' hide enumFromString, enumToString;
+export 'package:flutter_multi_formatter/flutter_multi_formatter.dart'
+    hide enumFromString, enumToString;
 export 'package:data_access/data_access.dart';
 export 'package:web_socket_channel/web_socket_channel.dart';
 export 'package:authentication/authentication.dart';
+export 'package:location_manager/location_manager.dart';
