6735de342979eda2ce21a57290428329a30b918d
Sita Bérété <sita.berete.3@gmail.com>
2021-12-17 01:07:17 +0000

    [dap] add getRating() to ReviewRepository

--- source_code/frontend/mobile/packages/data_access/lib/src/repositories/review_repository.dart
+++ source_code/frontend/mobile/packages/data_access/lib/src/repositories/review_repository.dart
@@ -1,7 +1,16 @@
 import 'package:data_access/src/repositories/base_repository.dart';
 import 'package:http_client/http_client.dart';
+import '../type_alias.dart';
 
 class ReviewRepository extends BaseRepository {
   ReviewRepository([HttpClient? httpClient])
       : super("/review", httpClient: httpClient);
+
+  Future<dynamic> getRating(JsonObject filter, String accessToken) async {
+    final response = await httpClient.get(
+      getPathWithQueryParams(filter, "$entityPath/rating"),
+      headers: getHeaders(accessToken),
+    );
+    return response.data;
+  }
 }
