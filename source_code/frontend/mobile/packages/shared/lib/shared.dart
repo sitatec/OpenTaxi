b9f8e223abaea719c8a139ef7efea4bb7120b736
Sita Bérété <sita.berete.3@gmail.com>
2021-12-20 00:23:27 +0000

    [shared] move trip_room package to shared package and make it a module

--- source_code/frontend/mobile/packages/shared/lib/shared.dart
+++ source_code/frontend/mobile/packages/shared/lib/shared.dart
@@ -5,6 +5,7 @@ export 'src/constants/values.dart';
 export 'src/utils/enum_utils.dart';
 export 'src/simple_widgets.dart';
 export 'src/entities/car.dart';
+export 'src/modules/trip_room/api.dart' hide TripRoomImplementation;
 // ----------------------- Exported Packages -------------------------- //
 export 'package:flutter_svg/flutter_svg.dart';
 export 'package:flutter_multi_formatter/flutter_multi_formatter.dart'
---
+++ source_code/frontend/mobile/packages/shared/lib/src/modules/trip_room/api.dart
@@ -0,0 +1,68 @@
+import 'package:meta/meta.dart';
+import 'dart:async';
+
+import 'package:firebase_database/firebase_database.dart';
+
+part 'trip_room_implementation.dart';
+
+abstract class TripRoom {
+  final String id;
+  final Stream<Location>? _locationSourceStream;
+  final Stream<double>? _speedSourceStream;
+
+  Stream<Location> get locationStream;
+
+  Stream<double> get speedStream;
+
+  Stream<TripEvent> get tripEventsStream;
+
+  String get riderId;
+
+  String get driverId;
+
+  String? get viewerId;
+
+  factory TripRoom(
+    String id, {
+    Stream<Location>? locationSourceStream,
+    Stream<double>? speedSourceStream,
+  }) =>
+      TripRoomImplementation(
+        id,
+        locationSourceStream: locationSourceStream,
+        speedSourceStream: speedSourceStream,
+      );
+
+  TripRoom._internal(
+    this.id,
+    this._locationSourceStream,
+    this._speedSourceStream,
+  );
+
+  void join();
+
+  Future<void> watch(String viewerId);
+
+  Future<void> leave();
+}
+
+class Location {
+  final double latitude;
+  final double longitude;
+
+  const Location({required this.latitude, required this.longitude});
+
+  static Location fromString(String location) {
+    final coordinates = location.split(',').map((e) => double.parse(e));
+    return Location(latitude: coordinates.first, longitude: coordinates.last);
+  }
+}
+
+enum TripEvent {
+  joined,
+  joinFailed,
+  viewerJoined,
+  cantWatchAlreadyJoinedTrip,
+  anotherViewerAlreadyWatching,
+  cantJoinTwice,
+}
