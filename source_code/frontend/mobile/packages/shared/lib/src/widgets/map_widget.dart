35010518ba9ed72b2357996850060d89cc8950f2
Sita Bérété <sita.berete.3@gmail.com>
2021-12-26 21:03:16 +0000

    [shared] add padding parameter to the MapWidget

--- source_code/frontend/mobile/packages/shared/lib/src/widgets/map_widget.dart
+++ source_code/frontend/mobile/packages/shared/lib/src/widgets/map_widget.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapWidget extends StatefulWidget {
-  const MapWidget({Key? key}) : super(key: key);
+  final EdgeInsets padding;
+  const MapWidget({Key? key, this.padding = EdgeInsets.zero}) : super(key: key);
 
   @override
   _MapWidgetState createState() => _MapWidgetState();
@@ -27,6 +28,7 @@ class _MapWidgetState extends State<MapWidget> {
   @override
   Widget build(BuildContext context) {
     return GoogleMap(
+      padding: widget.padding,
       initialCameraPosition: _kGooglePlex,
       onMapCreated: (GoogleMapController controller) {
         _controller.complete(controller);
