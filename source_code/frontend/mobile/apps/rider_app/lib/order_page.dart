169670b0a8ac516e32d82500cd0896b07468569b
Sita Bérété <sita.berete.3@gmail.com>
2021-12-27 22:32:02 +0000

    [rid] build 'Searching for driver' widget

--- source_code/frontend/mobile/apps/rider_app/lib/order_page.dart
+++ source_code/frontend/mobile/apps/rider_app/lib/order_page.dart
@@ -10,6 +10,7 @@ class OrderPage extends StatefulWidget {
 
 class _OrderPageState extends State<OrderPage> {
   String selectedCar = _CarInfo.economy.type;
+  bool isSearchingForDriver = false;
 
   @override
   Widget build(BuildContext context) {
@@ -41,119 +42,152 @@ class _OrderPageState extends State<OrderPage> {
                 color: Colors.white,
                 borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
               ),
-              child: Wrap(
-                children: [
-                  ListTile(
-                    leading: CircleAvatar(
-                      child: SvgPicture.asset("assets/images/pickup_icon.svg",
-                          package: "shared"),
-                      backgroundColor: iconsBackgroundColor,
-                    ),
-                    title: Text(
-                      "PICK UP",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 11,
-                        color: theme.disabledColor,
-                      ),
-                    ),
-                    subtitle: Text("bookingRequestData.pickUpAddress"),
-                  ),
-                  ListTile(
-                    leading: CircleAvatar(
-                      child: SvgPicture.asset("assets/images/dropoff_icon.svg",
-                          package: "shared"),
-                      backgroundColor: iconsBackgroundColor,
-                    ),
-                    title: Text(
-                      "DROP OFF",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 11,
-                        color: theme.disabledColor,
-                      ),
-                    ),
-                    subtitle: Text("bookingRequestData.dropOfAddress"),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 16),
-                    child: TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        "+ Add stop",
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      style: TextButton.styleFrom(padding: EdgeInsets.zero),
-                    ),
-                  ),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                        left: 16,
-                        bottom: 20,
-                        top: 8,
-                      ),
-                      child: Row(
-                        children: [
-                          _CarInfoWidget(
-                            _CarInfo.economy,
-                            isSelected: selectedCar == _CarInfo.economy.type,
-                            onClicked: () => setState(
-                              () => selectedCar = _CarInfo.economy.type,
-                            ),
-                          ),
-                          const SizedBox(width: 8),
-                          _CarInfoWidget(
-                            _CarInfo.minivan,
-                            isSelected: selectedCar == _CarInfo.minivan.type,
-                            onClicked: () => setState(
-                              () => selectedCar = _CarInfo.minivan.type,
-                            ),
-                          ),
-                          const SizedBox(width: 8),
-                          _CarInfoWidget(
-                            _CarInfo.comfort,
-                            isSelected: selectedCar == _CarInfo.comfort.type,
-                            onClicked: () => setState(
-                              () => selectedCar = _CarInfo.comfort.type,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Row(
+              child: isSearchingForDriver
+                  ? Wrap(
+                      crossAxisAlignment: WrapCrossAlignment.center,
+                      direction: Axis.vertical,
                       children: [
-                        IconButton(
-                          padding: EdgeInsets.only(
-                              top: 8, bottom: 8, left: 8, right: 24),
-                          icon: Icon(Icons.arrow_back),
-                          onPressed: () => Navigator.of(context).pop(),
+                        const SizedBox(height: 30),
+                        CircularProgressIndicator(
+                          color: theme.primaryColor.withAlpha(200),
                         ),
-                        Expanded(
-                          child: RoundedCornerButton(
+                        Container(
+                          padding: const EdgeInsets.all(20),
+                          width: MediaQuery.of(context).size.width,
+                          child: Text(
+                            "Searching for driver...",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: theme.primaryColor,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  : Wrap(
+                      children: [
+                        ListTile(
+                          leading: CircleAvatar(
+                            child: SvgPicture.asset(
+                                "assets/images/pickup_icon.svg",
+                                package: "shared"),
+                            backgroundColor: iconsBackgroundColor,
+                          ),
+                          title: Text(
+                            "PICK UP",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 11,
+                              color: theme.disabledColor,
+                            ),
+                          ),
+                          subtitle: Text("bookingRequestData.pickUpAddress"),
+                        ),
+                        ListTile(
+                          leading: CircleAvatar(
+                            child: SvgPicture.asset(
+                                "assets/images/dropoff_icon.svg",
+                                package: "shared"),
+                            backgroundColor: iconsBackgroundColor,
+                          ),
+                          title: Text(
+                            "DROP OFF",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 11,
+                              color: theme.disabledColor,
+                            ),
+                          ),
+                          subtitle: Text("bookingRequestData.dropOfAddress"),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 16),
+                          child: TextButton(
                             onPressed: () {},
                             child: const Text(
-                              "Order",
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.w500,
-                                color: Colors.white,
-                              ),
-                              textAlign: TextAlign.center,
+                              "+ Add stop",
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            style:
+                                TextButton.styleFrom(padding: EdgeInsets.zero),
+                          ),
+                        ),
+                        SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                              left: 16,
+                              bottom: 20,
+                              top: 8,
+                            ),
+                            child: Row(
+                              children: [
+                                _CarInfoWidget(
+                                  _CarInfo.economy,
+                                  isSelected:
+                                      selectedCar == _CarInfo.economy.type,
+                                  onClicked: () => setState(
+                                    () => selectedCar = _CarInfo.economy.type,
+                                  ),
+                                ),
+                                const SizedBox(width: 8),
+                                _CarInfoWidget(
+                                  _CarInfo.minivan,
+                                  isSelected:
+                                      selectedCar == _CarInfo.minivan.type,
+                                  onClicked: () => setState(
+                                    () => selectedCar = _CarInfo.minivan.type,
+                                  ),
+                                ),
+                                const SizedBox(width: 8),
+                                _CarInfoWidget(
+                                  _CarInfo.comfort,
+                                  isSelected:
+                                      selectedCar == _CarInfo.comfort.type,
+                                  onClicked: () => setState(
+                                    () => selectedCar = _CarInfo.comfort.type,
+                                  ),
+                                )
+                              ],
                             ),
                           ),
-                        )
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(16),
+                          child: Row(
+                            children: [
+                              IconButton(
+                                padding: const EdgeInsets.only(
+                                  top: 8,
+                                  bottom: 8,
+                                  left: 8,
+                                  right: 24,
+                                ),
+                                icon: const Icon(Icons.arrow_back),
+                                onPressed: () => Navigator.of(context).pop(),
+                              ),
+                              Expanded(
+                                child: RoundedCornerButton(
+                                  onPressed: () {},
+                                  child: const Text(
+                                    "Order",
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.white,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
                       ],
                     ),
-                  ),
-                ],
-              ),
             ),
-          )
+          ),
         ],
       ),
     );
