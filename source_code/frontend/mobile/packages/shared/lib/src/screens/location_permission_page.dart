3b6dec4b1c782417e285e586fadb227b14908532
Sita Bérété <sita.berete.3@gmail.com>
2021-12-30 01:06:20 +0000

    [shared] update LocationPermissionPage to support been shown in a dialog

--- source_code/frontend/mobile/packages/shared/lib/src/screens/location_permission_page.dart
+++ source_code/frontend/mobile/packages/shared/lib/src/screens/location_permission_page.dart
@@ -13,73 +13,69 @@ class LocationPermissionPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final contentWidth = MediaQuery.of(context).size.width * 0.8;
-    return Scaffold(
-      body: Center(
-        child: Wrap(
-          direction: Axis.vertical,
-          crossAxisAlignment: WrapCrossAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 50,
-              child: const Icon(
-                Icons.location_on_rounded,
-                color: Colors.white,
-                size: 50,
-              ),
-              backgroundColor: gray.withAlpha(60),
-            ),
-            const SizedBox(height: 25),
-            const Text(
-              "Enable Location",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 16, bottom: 50),
-              width: contentWidth,
-              child: description ??
-                  RichText(
-                    textAlign: TextAlign.center,
-                    text: const TextSpan(
-                        text:
-                            'Your location will be used to send you requests from the closest riders. In the next step click on "',
-                        style: TextStyle(color: gray, fontSize: 15),
+    final contentWidth = MediaQuery.of(context).size.width * 0.7;
+    return Wrap(
+      direction: Axis.vertical,
+      crossAxisAlignment: WrapCrossAlignment.center,
+      children: [
+        CircleAvatar(
+          radius: 50,
+          child: const Icon(
+            Icons.location_on_rounded,
+            color: Colors.white,
+            size: 50,
+          ),
+          backgroundColor: gray.withAlpha(60),
+        ),
+        const SizedBox(height: 25),
+        const Text(
+          "Enable Location",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 16, bottom: 50),
+          width: contentWidth,
+          child: description ??
+              RichText(
+                textAlign: TextAlign.center,
+                text: const TextSpan(
+                    text:
+                        'Your location will be used to send you requests from the closest riders. In the next step click on "',
+                    style: TextStyle(color: gray, fontSize: 15),
+                    children: [
+                      TextSpan(
+                        text: 'Allways',
+                        style: TextStyle(fontWeight: FontWeight.bold),
                         children: [
                           TextSpan(
-                            text: 'Allways',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                            children: [
-                              TextSpan(
-                                text:
-                                    "\" otherwise we won't have the required permission.",
-                                style: TextStyle(fontWeight: FontWeight.normal),
-                              )
-                            ],
+                            text:
+                                "\" otherwise we won't have the required permission.",
+                            style: TextStyle(fontWeight: FontWeight.normal),
                           )
-                        ]),
-                  ),
-            ),
-            SizedBox(
-              width: contentWidth,
-              child: RoundedCornerButton(
-                child: const Text(
-                  "Enable",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.white,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                onPressed: onEnableButtonClicked,
+                        ],
+                      )
+                    ]),
               ),
-            )
-          ],
         ),
-      ),
+        SizedBox(
+          width: contentWidth,
+          child: RoundedCornerButton(
+            child: const Text(
+              "Enable",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w700,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            onPressed: onEnableButtonClicked,
+          ),
+        )
+      ],
     );
   }
 }
