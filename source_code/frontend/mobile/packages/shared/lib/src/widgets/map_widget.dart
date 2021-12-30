4679f9a8c29d7ed09a2c4c2200848512ff5baed6
Sita Bérété <sita.berete.3@gmail.com>
2021-12-30 18:54:49 +0000

    [shared] complete MapWidget

--- source_code/frontend/mobile/packages/shared/lib/src/widgets/map_widget.dart
+++ source_code/frontend/mobile/packages/shared/lib/src/widgets/map_widget.dart
@@ -1,43 +1,95 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapWidget extends StatefulWidget {
   final EdgeInsets padding;
-  const MapWidget({Key? key, this.padding = EdgeInsets.zero}) : super(key: key);
+  final Map<String, String> encodedPolylines;
+  final Completer<GoogleMapController> controller;
+  final Set<Marker> markers;
+  const MapWidget({
+    Key? key,
+    this.padding = EdgeInsets.zero,
+    this.encodedPolylines = const {},
+    required this.controller,
+    this.markers = const {},
+  }) : super(key: key);
 
   @override
   _MapWidgetState createState() => _MapWidgetState();
 }
 
 class _MapWidgetState extends State<MapWidget> {
-  Completer<GoogleMapController> _controller = Completer();
-
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
+  static const CameraPosition _southAfrica = CameraPosition(
+    target: LatLng(-34.2973267, 18.252956),
+    zoom: 5,
   );
-
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
 
   @override
   Widget build(BuildContext context) {
     return GoogleMap(
       padding: widget.padding,
-      initialCameraPosition: _kGooglePlex,
+      initialCameraPosition: _southAfrica,
+      zoomControlsEnabled: false,
+      markers: widget.markers,
+      polylines: _getPolylines(),
       onMapCreated: (GoogleMapController controller) {
-        _controller.complete(controller);
+        widget.controller.complete(controller);
       },
     );
   }
 
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  Set<Polyline> _getPolylines() {
+    final polylines = <Polyline>{};
+    widget.encodedPolylines.forEach((polylineId, encodedPolyline) {
+      polylines.add(
+        Polyline(
+          polylineId: PolylineId(polylineId),
+          points: _decodePolyline(encodedPolyline),
+        ),
+      );
+    });
+    return polylines;
   }
+}
+
+List<LatLng> _decodePolyline(String input) {
+  var list = input.codeUnits;
+  List lList = [];
+  int index = 0;
+  int len = input.length;
+  int c = 0;
+  List<LatLng> positions = [];
+  // repeating until all attributes are decoded
+  do {
+    var shift = 0;
+    int result = 0;
+
+    // for decoding value of one attribute
+    do {
+      c = list[index] - 63;
+      result |= (c & 0x1F) << (shift * 5);
+      index++;
+      shift++;
+    } while (c >= 32);
+    /* if value is negetive then bitwise not the value */
+    if (result & 1 == 1) {
+      result = ~result;
+    }
+    var result1 = (result >> 1) * 0.00001;
+    lList.add(result1);
+  } while (index < len);
+
+  /*adding to previous value as done in encoding */
+  for (int i = 2; i < lList.length; i++) {
+    lList[i] += lList[i - 2];
+  }
+
+  for (int i = 0; i < lList.length; i += 2) {
+    positions.add(LatLng(lList[i], lList[i + 1]));
+  }
+
+  return positions;
 }
