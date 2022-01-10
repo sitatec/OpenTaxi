06ba16cce3bcc170336e862cfb43849abb5ce620
Sita Bérété <sita.berete.3@gmail.com>
2022-01-10 07:19:00 +0000

    [com] add sendTextMessage and sendFileMessage method

--- source_code/frontend/mobile/packages/communication/lib/src/domain/communication_manager.dart
+++ source_code/frontend/mobile/packages/communication/lib/src/domain/communication_manager.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:communication/src/domain/config.dart';
 import 'package:communication/src/domain/models.dart';
 import 'package:sendbird_sdk/constant/enums.dart';
@@ -7,16 +10,19 @@ import 'package:sendbird_sdk/sdk/sendbird_sdk_api.dart';
 import 'package:sendbird_sdk/sendbird_sdk.dart';
 
 class ComunicationManager {
-  final InterlocutorsData _interlocutorsData;
+  final CommunicationChannelData _channelData;
   SendbirdSdk? _sendbirdSdk;
+  GroupChannel? comunicationChannel;
+  bool isInitialized = false;
 
-  ComunicationManager(this._interlocutorsData, {SendbirdSdk? sendbirdSdk})
+  ComunicationManager(this._channelData, {SendbirdSdk? sendbirdSdk})
       : _sendbirdSdk = sendbirdSdk;
 
   Future<void> initialize() async {
     try {
       _sendbirdSdk ??= SendbirdSdk(appId: sendbirdAppId);
-      await _sendbirdSdk!.connect(_interlocutorsData.currentUserId);
+      await _sendbirdSdk!.connect(_channelData.currentUserId);
+      isInitialized = true;
     } catch (e) {
       //TODO
     } finally {
@@ -27,30 +33,75 @@ class ComunicationManager {
   Future<void> createChatChannel() async {
     try {
       final channelParams = GroupChannelParams()
+        ..channelUrl = _channelData.channelId
         ..isPublic = false
         ..isDistinct = true
-        ..userIds = _interlocutorsData.interlocutorsIds;
-      final channel = await GroupChannel.createChannel(channelParams);
-      await channel.join();
+        ..userIds = _channelData.interlocutorsIds;
+      comunicationChannel = await GroupChannel.createChannel(channelParams);
+      await comunicationChannel!.join();
     } catch (e) {
       // TODO
     }
   }
 
   Future<void> joinChatChannel() async {
+    if (comunicationChannel != null) {
+      return; // TODO already joinned
+    }
     try {
-      final channelQuery = GroupChannelListQuery()
-        ..userIdsExactlyIn = _interlocutorsData.interlocutorsIds;
-      final channel = await channelQuery.loadNext();
-      await channel.first.join();
+      comunicationChannel =
+          await GroupChannel.getChannel(_channelData.channelId);
+      await comunicationChannel!.join();
     } catch (e) {
       //TODO
     }
   }
 
-  Future<void> sendTextMessage(String message) async {}
+  Future<bool> sendTextMessage(String message) async {
+    if (comunicationChannel == null) {
+      return false; // TODO must join before been able to send messages
+    }
+    try {
+      final completer = Completer<bool>();
+      comunicationChannel!.sendUserMessageWithText(message,
+          onCompleted: (message, error) {
+        if (error != null) {
+          print(error); //TODO handle
+        }
+        completer
+            .complete(message.sendingStatus == MessageSendingStatus.succeeded);
+      });
+      return completer.future;
+    } catch (e) {
+      print(e);
+      rethrow; //TODO handle
+    }
+  }
+
+  Future<bool> sendFileMessage(File file) async {
+    if (comunicationChannel == null) {
+      return false; // TODO must join before been able to send messages
+    }
+    try {
+      final completer = Completer<bool>();
+      comunicationChannel!.sendFileMessage(FileMessageParams.withFile(file),
+          onCompleted: (message, error) {
+        if (error != null) {
+          print(error); //TODO handle
+        }
+        completer
+            .complete(message.sendingStatus == MessageSendingStatus.succeeded);
+      });
+      return completer.future;
+    } catch (e) {
+      print(e);
+      rethrow; //TODO handle
+    }
+  }
 
   Future<void> makeAudioCall() async {}
 
-  Future<void> dispose() async {}
+  Future<void> dispose() async {
+    _sendbirdSdk?.disconnect();
+  }
 }
--- source_code/frontend/mobile/packages/communication/lib/src/domain/models.dart
+++ source_code/frontend/mobile/packages/communication/lib/src/domain/models.dart
@@ -1,13 +1,17 @@
-class InterlocutorsData {
+class CommunicationChannelData {
   final String currentUserId;
   final String remoteUserId;
   final String remoteUserName;
-
+  late final String channelId;
   List<String> get interlocutorsIds => [currentUserId, remoteUserId];
 
-  InterlocutorsData({
+  CommunicationChannelData({
     required this.currentUserId,
     required this.remoteUserId,
     required this.remoteUserName,
-  });
+  }) {
+    interlocutorsIds.sort();
+    // We sort the ids to make sure that the final string below is identic in both remote and current user side.
+    channelId = interlocutorsIds.join();
+  }
 }
