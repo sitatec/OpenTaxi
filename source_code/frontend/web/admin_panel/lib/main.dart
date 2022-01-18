d3c0820b1e415a8aa7578e3432c351ecc97edb8d
Sita Bérété <sita.berete.3@gmail.com>
2022-01-18 21:10:35 +0000

    [admin] setup firebase

--- source_code/frontend/web/admin_panel/lib/main.dart
+++ source_code/frontend/web/admin_panel/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:admin_panel/pages/driver_registration_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'config/firebase_options.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,64 +13,80 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Hamba Admin',
-      initialRoute: "/driver-registration",
       onGenerateRoute: Router.generateRoute,
+      home: FutureBuilder(
+          future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
+          ),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return Router.wrappePageInDefaultScaffold(
+                const DriverRegistrationPage(),
+              );
+            }
+            return const Center(child: Text("Loading..."));
+          }),
     );
   }
 }
 
 class Router {
   static Route<dynamic> generateRoute(RouteSettings settings) {
-    Widget? page;
+    late Widget page;
     switch (settings.name) {
       case "/driver-registration":
         page = const DriverRegistrationPage();
         break;
       default:
     }
+
     return MaterialPageRoute(builder: (context) {
-      return Scaffold(
-        appBar: AppBar(
-          titleSpacing: 0,
-          toolbarHeight: 50,
-          title: const Text("Hamba Admin", textScaleFactor: 0.95),
-          actions: [
-            InkWell(
-              onTap: () {},
-              child: const CircleAvatar(
-                radius: 19,
-                child: Icon(
-                  Icons.person,
-                  size: 20,
-                ),
+      return wrappePageInDefaultScaffold(page);
+    });
+  }
+
+  static Widget wrappePageInDefaultScaffold(Widget page) {
+    return Scaffold(
+      appBar: AppBar(
+        titleSpacing: 0,
+        toolbarHeight: 50,
+        title: const Text("Hamba Admin", textScaleFactor: 0.95),
+        actions: [
+          InkWell(
+            onTap: () {},
+            child: const CircleAvatar(
+              radius: 19,
+              child: Icon(
+                Icons.person,
+                size: 20,
               ),
             ),
-            const SizedBox(width: 16),
-          ],
-        ),
-        body: page,
-        drawer: Drawer(
-          child: SingleChildScrollView(
-            child: Column(
-              children: const [
-                Text("Destination 1"),
-                SizedBox(height: 8),
-                Text("Destination 2"),
-                SizedBox(height: 8),
-                Text("Destination 3"),
-                SizedBox(height: 8),
-                Text("Destination 4"),
-                SizedBox(height: 8),
-                Text("Destination 5"),
-                SizedBox(height: 8),
-              ],
-            ),
+          ),
+          const SizedBox(width: 16),
+        ],
+      ),
+      body: page,
+      drawer: Drawer(
+        child: SingleChildScrollView(
+          child: Column(
+            children: const [
+              Text("Destination 1"),
+              SizedBox(height: 8),
+              Text("Destination 2"),
+              SizedBox(height: 8),
+              Text("Destination 3"),
+              SizedBox(height: 8),
+              Text("Destination 4"),
+              SizedBox(height: 8),
+              Text("Destination 5"),
+              SizedBox(height: 8),
+            ],
           ),
         ),
-      );
-    });
+      ),
+    );
   }
 }
